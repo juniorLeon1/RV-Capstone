ba6c24ddb77fdd59bafc29f44cd27822ce088767
juniorLeon1 <leon71314@gmail.com>
2023-04-20 22:57:11 -0400

    navbar update

---
+++ src/components/UI/Dropdown.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/Dropdown.css";
+
+const Dropdown = (props) => {
+  return (
+    <div className={`drpdwn-menu-wrap ${props.toggled? 'active' : 'inactive' }`}>
+      <ul className="drpdwn-menu">
+        {props.menu.map((element) => {
+          return (
+            <Link to={element.url}>
+              <li key={element.item} className="drpdwn-item">{element.item}</li>
+            </Link>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Dropdown;
