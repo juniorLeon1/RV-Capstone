f45da9a01a077c058b7875fd22f7626492f8370a
Ching Vang <chingvaj1@gmail.com>
2023-04-22 17:12:13 -0400

    creation of database

--- database/creation.sql
+++ database/creation.sql
@@ -1,12 +1,19 @@
-create database avoTravelerDB;
+-- create database avoTravelerDB;
 use avoTravelerDB;
 
-create table destination(
-id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(25),
-discription VARCHAR(100),
-img VARCHAR(25),
-longitude decimal(4, 6),
-latitude decimal(4, 6)
-);
+-- create table destination(
+-- id INT AUTO_INCREMENT PRIMARY KEY,
+-- location VARCHAR(25),
+-- img VARCHAR(45),
+-- longitude decimal(10, 6),
+-- latitude decimal(10, 6)
+-- );
 
+--  insert into destination (location, img, longitude,latitude) values
+-- 	('Charlotte, USA','../../images/charlotteSkyline.png',-80.841141,35.223789),
+--     ('London, UK','../../images/londonSkyline.png',-0.127758,51.507351),
+--     ('San Juan, Puerto Rico','../../images/sanjuanSkyline.png',-66.105721,18.466333),
+--     ('São Paulo, Brazil','../../images/saopauloSkyline.png',-46.625290,-23.533773);
+
+ALTER USER 'chingvang'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON destination.* TO 'chingvang'@'localhost';
