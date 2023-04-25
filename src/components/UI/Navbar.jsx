5e9a2db9fcf14054b740a4df65a85278620c5cc8
JosephVang099 <joseph-vang@outlook.com>
2023-04-25 13:46:55 -0400

    Update Navbar.jsx

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,25 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/Navbar.css";
+import Dropdown from "./Dropdown";
 
 function Navbar() {
-  return (
-    <nav className="navbar">
-      <h1>social planet</h1>
+  const [destDrpdwnToggle, setDestDrpdwnToggle] = useState(false);
 
-      <Link to="/">
-        <img className="world-logo" alt="our brand name's logo" src={require("../../images/World.png")} />
+  return (
+    <nav className="navbar-container">
+      {/* left side */}
+      <h1 className="web-name">AvoTraveler</h1>
+
+      {/* center */}
+      <Link to="/" className="logo-link">
+        <img
+          className="world-logo"
+          alt="AvoTraveler logo"
+          src={require("../../images/World.png")}
+        />
       </Link>
 
-      <ul className="nav-links">
-        <li>
-          <Link to="/login">Login</Link>
+      {/* right side */}
+      <ul className="navbar-links-container">
+        
+        <li className="navigation-item">
+          <Link to="/login" className="navigation-link">
+            Login
+          </Link>
         </li>
-        <li>
-          <Link to="/destinations">Destinations</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
+
+        <li
+          className="navigation-item"
+          onMouseLeave={() => {
+            setDestDrpdwnToggle(!destDrpdwnToggle);
+          }}
+        >
+          <Link
+            to="/destinations"
+            className="navigation-link"
+            onMouseEnter={() => {
+              setDestDrpdwnToggle(!destDrpdwnToggle);
+            }}
+          >
+            Destinations{" "}
+            <i
+              className={`fa-solid fa-caret-down fa-rotate-90 drpdwn-arrow ${
+                destDrpdwnToggle ? "active" : "inactive"
+              }`}
+            ></i>
+          </Link>
+          <Dropdown
+            toggled={destDrpdwnToggle}
+            menu={[
+              {
+                item: "Brazil",
+                url: "/destinations/brazil",
+              },
+              {
+                item: "Charlotte",
+                url: "/destinations/charlotte",
+              },
+              {
+                item: "London",
+                url: "/destinations/london",
+              },
+              {
+                item: "Puerto Rico",
+                url: "/destinations/puerto-rico",
+              },
+            ]}
+          />
         </li>
       </ul>
     </nav>
