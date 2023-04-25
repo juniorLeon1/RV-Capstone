8852bf346a399a0468cdcdfb454030414e80c010
JosephVang099 <joseph-vang@outlook.com>
2023-04-25 16:01:17 -0400

    Brazil Changes

--- src/pages/Destinations/Brazil.jsx
+++ src/pages/Destinations/Brazil.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../../styles/Brazil.css"
 
 const Brazil = () => {
   return (
@@ -12,25 +13,26 @@ const Brazil = () => {
           <h2>Eco-Friendly Places</h2>
         </section>
         <section>
-          <img src={require("../../images/RV_Freedom_Park.png")} alt='' className='img' />
-          <h2>Freedom Park</h2>
-          <p>Freedom Park is a 98-acre park in Charlotte, North Carolina. The park has paved trails, tennis/volleyball courts, sport/athletic fields and playground equipment.</p>
+          <img src={require("../../images/RV_Pantanal.avif")} alt='' className='img' />
+          <h2>Pantanal</h2>
+          <p>The Pantanal has the greatest concentration of wildlife in the Americas.</p>
         </section>
         <section>
-          <img src={require("../../images/RV_Wing_Haven.jpeg")} alt='' className='img' />
-          <h2>Wing Haven</h2>
-          <p>A respite for birds and other wildlife and a welcoming place to discover the beauty and wonders of nature.</p>
+          <img src={require("../../images/RV_Foz_do_Iguassa.jpeg")} alt='' className='img' />
+          <h2>Foz do Iguassu</h2>
+          <p>Iguassu Falls is a truly spectacular sight, one of South America's truly iconic places.</p>
         </section>
         <section>
-          <img src={require("../../images/RV_McDowell.webp")} alt='' className='img' />
-          <h2>McDowell Nature Center and Preserve</h2>
-          <p>Provides educational programs and resources at the County’s oldest and largest nature preserve. </p>
+          <img src={require("../../images/RV_Rio_de_Janeiro.jpeg")} alt='' className='img' />
+          <h2>Rio de Janeiro</h2>
+          <p>Known to Brazilians as 'the marvellous city', Rio de Janeiro has undeniably the most beautiful setting of any city in the world.</p>
         </section>
       </div>
+
       <div className="side-content">
         <section>
-          <img src={require("../../images/RV_Brazil.jpeg")} alt='' className='side-img' />
-          <h2 className='side-content-title'>Brazil</h2>
+          <img src={require("../../images/RV_Charlotte.jpeg")} alt='' className='side-img' />
+          <h2 className='side-content-title'>Charlotte</h2>
           <p>Why designing for mobile devices first can help you create more effective and user-friendly websites.</p>
         </section>
         <section>
@@ -44,6 +46,7 @@ const Brazil = () => {
           <p>Examples and best practices for creating responsive navigation menus that work well on both desktop and mobile devices.</p>
         </section>
       </div>
+
       <div className="small-content">
         <section className='small-content-title'>
           <h2>Eco-Friendly Resturants</h2>
@@ -57,24 +60,24 @@ const Brazil = () => {
           </ul>
         </section> */}
         <section className='food-container'>
-          <img src={require("../../images/RV_Flower_Child.jpeg")} alt='' className='img' />
+          <img src={require("../../images/RV_Lagoa_Tropical.jpeg")} alt='' className='img' />
           <section className='food-content'>
-            <h2>Flower Child</h2>
-            <p>On a simple, soul-satisfying mission to serve positively delicious vibes to the Charlotte community. Chock full of nourishing goodness our made-from-scratch menu caters to all lifestyles from vegetarian and vegan, to meat-eating and gluten-free to keto and paleo.</p>
+            <h2>Lagoa Tropical</h2>
+            <p>Vegetarian buffet restaurant with diverse fare such as salads, quiches, pasta & stews.</p>
           </section>
         </section>
         <section className='food-container'>
-          <img src={require("../../images/RV_Living_Kitchen.jpeg")} alt='' className='img' />
+          <img src={require("../../images/RV_Naturalie_Bistro.jpeg")} alt='' className='img' />
           <section className='food-content'>
-            <h2>Living Kitchen</h2>
-            <p>Living Kitchen is your local hot spot for organic plant-based cuisine. Our vegan food creations are enjoyed by everyone!</p>
+            <h2>Naturalie Bistrô</h2>
+            <p>Natural food restaurant offering seasonal menus according to produce availability</p>
           </section>
         </section>
         <section className='food-container'>
-          <img src={require("../../images/RV_Bean_Vegan.jpg")} alt='' className='img' />
+          <img src={require("../../images/RV_Teva.jpeg")} alt='' className='img' />
           <section className='food-content'>
-            <h2>Bean Vegan Cuisine</h2>
-            <p>100% vegan comfort foods! From burgers to breakfast, BBQ to buffalo fingers, and everything in between, our menu offers the kind of variety vegans in the area crave. We create fantastic flavors that vegans and non-vegans alike will love. Come on in and experience it for yourself. It’s good y’all!</p>
+            <h2>Teva</h2>
+            <p>Teva is a vegetable bar and restaurant offering a delicious menu and a unique experience, raising people's awareness of their impact on the world .</p>
           </section>
         </section>
       </div>
