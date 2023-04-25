f65df79317f05f9f564cf3650f3d840cf2a27c33
capellandiana <capellandiana@yahoo.com>
2023-04-25 08:56:09 -0400

    final section of homepage added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,19 @@ import Hero from '../components/Home/Hero'
 import Plan from '../components/Home/Plan'
 import CircleSmall from '../components/Home/CircleSmall'
 import Featured from '../components/Home/Featured'
+import Guides from '../components/Home/Guides'
 
 
 const Home = () => {
   return (
     <div>
-      <Hero />
+      {/* <Hero />
       <Experience />
       <CircleSmall />
       <Plan />
       <Featured />
-      <h1>Home</h1>
+      <Guides />
+      <h1>Home</h1> */}
     </div>
   )
 }
