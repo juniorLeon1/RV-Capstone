7d7f83e91e5e7711353c24b9432a3a4022fd48f8
JosephVang099 <joseph-vang@outlook.com>
2023-04-25 13:02:42 -0400

    Update Footer.jsx

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+import "../../styles/Footer.css";
 
 function Footer() {
   return (
     <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-        <div className='me-5 d-none d-lg-block'>
+        <div className='text-dark'>
           <span>Get connected with us on social networks:</span>
         </div>
 
         <div>
-          <a href='https://www.facebook.com/' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='facebook-f' />
+          <a href='https://www.facebook.com/' className='me-4 text-reset' style={{ color: 'red' }}>
+            <MDBIcon color='primary' fab icon='facebook-f' />
           </a>
           <a href='https://twitter.com/?lang=en' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='twitter' />
+            <MDBIcon color='primary' fab icon='twitter' />
           </a>
           <a href='https://www.instagram.com/' className='me-4 text-reset'>
-            <MDBIcon color='secondary' fab icon='instagram' />
+            <MDBIcon color='warning' fab icon='instagram' />
           </a>
         </div>
       </section>
@@ -26,36 +27,36 @@ function Footer() {
         <MDBContainer className='text-center text-md-start mt-5'>
           <MDBRow className='mt-3'>
             <MDBCol md='3' lg='4' xl='3' className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon color='secondary' icon='gem' className='me-3' />
-                Company name
+              <h6 className='text-primary fw-bold mb-4'>
+                <MDBIcon color='primary' icon='gem' className='me-3' />
+                AvoTravler
               </h6>
-              <p>
+              <p className="custom-color">
                 Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit
                 amet, consectetur adipisicing elit.
               </p>
             </MDBCol>
 
             <MDBCol md='2' lg='2' xl='2' className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+              <h6 className='text-primary fw-bold mb-4'>DESTINATION</h6>
               <p>
-                <a href='#!' className='text-reset'>
-                  Angular
+                <a href='#!' className="custom-color">
+                  Charlotte
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  React
+                <a href='#!' className="custom-color">
+                  Brazil
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  Vue
+                <a href='#!' className="custom-color">
+                Puerto Rico
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  Laravel
+                <a href='#!' className="custom-color">
+                  London
                 </a>
               </p>
             </MDBCol>
@@ -85,20 +86,20 @@ function Footer() {
             </MDBCol>
 
             <MDBCol md='4' lg='3' xl='3' className='mx-auto mb-md-0 mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
-              <p>
-                <MDBIcon color='secondary' icon='home' className='me-2' />
+              <h6 className='text-primary fw-bold mb-4'>CONTACT</h6>
+              <p className="text-dark">
+                <MDBIcon color='dark' icon='home' className='me-2' />
                 New York, NY 10012, US
               </p>
-              <p>
-                <MDBIcon color='secondary' icon='envelope' className='me-3' />
+              <p className="text-dark">
+                <MDBIcon color='dark' icon='envelope' className='me-3' />
                 info@example.com
               </p>
-              <p>
-                <MDBIcon color='secondary' icon='phone' className='me-3' /> + 01 234 567 88
+              <p className="text-dark">
+                <MDBIcon color='dark' icon='phone' className='me-3' /> + 01 234 567 88
               </p>
-              <p>
-                <MDBIcon color='secondary' icon='print' className='me-3' /> + 01 234 567 89
+              <p className="text-dark">
+                <MDBIcon color='dark' icon='print' className='me-3' /> + 01 234 567 89
               </p>
             </MDBCol>
           </MDBRow>
@@ -108,7 +109,7 @@ function Footer() {
       <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
         © 2021 Copyright:
         <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
+          AvoTravler
         </a>
       </div>
     </MDBFooter>
