66c85892899e44b1d85e5b74f8091bcc1c7482b3
capellandiana <capellandiana@yahoo.com>
2023-04-27 19:55:46 -0400

    destination page progress

--- src/components/Destinations components/Destination Cards.jsx
+++ src/components/Destinations components/Destination Cards.jsx
@@ -1,51 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import "../../styles/Destinations.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import "../../styles/Destinations.css";
 
 function DestinationCards() {
-  const [info,setInfo] = useState([]);
-  
-  useEffect(()=>{
-    callSql();
-  },[])
+  const [info, setInfo] = useState([]);
 
-  const callSql= async()=>{
-    try{
-     const response = await fetch ('http://localhost:8800/destinations');
-     const data = await response.json();
-     console.log('Fetch complete')
-     setInfo(data);
-    }catch(error){
-        console.error(error);
+  useEffect(() => {
+    callSql();
+  }, []);
+
+  const callSql = async () => {
+    try {
+      const response = await fetch("http://localhost:8800/destinations");
+      const data = await response.json();
+      console.log("Fetch complete");
+      setInfo(data);
+    } catch (error) {
+      console.error(error);
     }
-  }
+  };
 
   return (
-    <div className='stucture'>
-      {info.map((info)=>{
-        return(
-          <div key= {info.id} className="desti-card" style={{ width: '18rem' }}>
-
-          <div>
-          <img src={info.img} className="desti-image" alt={info.location} />
-          </div>
-          
-          <div>
-          <div className="">
-          <h5 className="">{info.location}</h5>
-          </div>
-
-          <div className="">
-          <Link to={info.pathing}><p>{info.location}</p></Link>
-          </div>
-          </div>
-          </div>
-        );
-      })}
-      
+    <div className="DestinationContainer">
+      {info.map((info) => (
+        <div className="item" key={info.id}>
+          {info.img && <img src={info.img} alt={info.location} />}
+          <h5>{info.location}</h5>
+          <Link to={info.pathing}>go here</Link>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default DestinationCards;
